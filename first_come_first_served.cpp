4b5cda9d0c2fa99d127e5114f7a276bc64e80476
Jay Sathiya <126950992+jaysathiya64@users.noreply.github.com>
2023-08-04 22:19:13 +0530

    Update first_come_first_served.cpp

--- first_come_first_served.cpp
+++ first_come_first_served.cpp
@@ -1,1 +1,32 @@
+#include <algorithm>
+#include <vector>
+#include "scheduling_process.cpp"
 
+void fcfs(vector<Process> &process) {
+    int time=0;
+    vector<char> ganttChart;
+
+    sort(process.begin(), process.end(), sortByArrival);
+
+    for(int i=0; i<process.size(); i++) {
+        while(time < process[i].getArrivalTime()) {
+            time++;
+            ganttChart.push_back('I');
+        }
+
+        time += process[i].getBurstTime();
+
+        for(int j=0; j<process[i].getBurstTime(); j++) {
+            ganttChart.push_back('0' + process[i].getPid());
+        }
+
+        process[i].setCompletionTime(time);
+        process[i].setTurnaroundTime(time - process[i].getArrivalTime());
+        process[i].setWaitingTime(process[i].getTurnaroundTime() - process[i].getBurstTime());
+    }
+
+    cout<<endl<<"Gantt Chart:"<<endl;
+    for(int i=0; i<ganttChart.size(); i++) cout<<ganttChart[i]<<" ";
+
+    displayProcessData(process);
+}
